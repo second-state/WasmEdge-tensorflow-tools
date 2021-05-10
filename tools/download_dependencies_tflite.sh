3475afbad8982cfdbb55775f13066668f2c6ea01
YiYing He <yiying@secondstate.io>
2021-05-10 15:50:00 +0800

    [Misc] Rename to WasmEdge.

--- tools/download_dependencies_tflite.sh
+++ tools/download_dependencies_tflite.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget https://github.com/second-state/ssvm-tensorflow-deps/releases/download/0.7.3/ssvm-tensorflow-deps-TFLite-0.7.3-manylinux2014_x86_64.tar.gz
-tar -zxvf ssvm-tensorflow-deps-TFLite-0.7.3-manylinux2014_x86_64.tar.gz
-rm -f ssvm-tensorflow-deps-TFLite-0.7.3-manylinux2014_x86_64.tar.gz
+wget https://github.com/second-state/WasmEdge-tensorflow-deps/releases/download/0.8.0/WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
+tar -zxvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
+rm -f WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
