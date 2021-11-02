7464547bad440d2190439d4b3c84df912e4e277e
YiYing He <yiying@secondstate.io>
2021-11-02 22:44:46 +0800

    [Tools] Remove download script.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- tools/download_dependencies_tflite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/second-state/WasmEdge-tensorflow-deps/releases/download/0.8.0/WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
-tar -zxvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
-rm -f WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
